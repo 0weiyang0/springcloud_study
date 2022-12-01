1ac65ef4b48c96c29a347bffbb7c206766597bd6
CarpeDiem <myCarpeDiem>
2022-12-01 14:33:19 +0800

    支付模块1

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZangNa
+ * @version 1.0
+ * @date 2022/12/1 13:54
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain8001.class,args);
+	}
+}
