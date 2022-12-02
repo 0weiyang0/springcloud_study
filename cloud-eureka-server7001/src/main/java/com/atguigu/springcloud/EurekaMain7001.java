826bf7752c25663c4940c2543f8ce2fb8187f022
CarpeDiem <myCarpeDiem>
2022-12-02 11:19:10 +0800

    Eureka Server

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ZangNa
+ * @version 1.0
+ * @date 2022/12/2 11:10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+	public static void main(String[] args) {
+
+		SpringApplication.run(EurekaMain7001.class,args);
+
+	}
+}
