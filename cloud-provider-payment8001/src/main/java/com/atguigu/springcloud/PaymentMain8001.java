e4bbed270721fdbcc7316b5d119eac2e6017ccaa
CarpeDiem <myCarpeDiem>
2022-12-02 13:01:40 +0800

    支付微服务8001入驻进eureka

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ZangNa
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/12/1 13:54
  */
 @SpringBootApplication
+@EnableEurekaClient//表示eureka client客户端
 public class PaymentMain8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentMain8001.class,args);
